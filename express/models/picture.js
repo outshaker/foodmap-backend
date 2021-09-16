cd40fad809c77433184c0c9eefff681a02170111
Sydars <xsw5888xsw@gmail.com>
2021-09-16 20:24:18 +0800

    後端食記(驗證部分未測試)

---
+++ express/models/picture.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Picture extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Picture.init({
+    post_id: DataTypes.INTEGER,
+    restaurant_id: DataTypes.INTEGER,
+    food_picture_url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Picture',
+  });
+  return Picture;
+};
